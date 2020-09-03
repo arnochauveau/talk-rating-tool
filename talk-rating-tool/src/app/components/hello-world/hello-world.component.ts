0ecd3febea045f01c1963ed0a6f2f04656842e70
arno chauveau <arnochauveau@gmail.com>
2020-09-03 13:49:15 +0200

    finished exercise 2

--- talk-rating-tool/src/app/components/hello-world/hello-world.component.ts
+++ talk-rating-tool/src/app/components/hello-world/hello-world.component.ts
@@ -1,10 +1,15 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-hello-world",
-  templateUrl: "./hello-world.component.html",
-  styleUrls: ["./hello-world.component.scss"],
+  selector: 'app-hello-world',
+  templateUrl: './hello-world.component.html',
+  styleUrls: ['./hello-world.component.scss'],
 })
 export class HelloWorldComponent {
-  title = "Hello World!";
+  title = 'Hello World!';
+  counter = 0;
+
+  IncreaseButtonClicked() {
+    this.counter++;
+  }
 }
